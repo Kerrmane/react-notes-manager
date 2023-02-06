a40a84054ea0e6836117959421ef8198a75b6a24
Aymane Kherrati <68582060+Kerrmane@users.noreply.github.com>
2023-02-07 00:58:58 +0100

    add router and create folder page

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { App } from "App";
 import { StrictMode } from "react";
+import {BrowserRouter, Routes,Route } from "react-router-dom";
+import {NoteBrowse} from "page/NoteBrowse/NoteBrowse";
+import {Note} from "page/Note/Note";
+import {NoteCreate} from "page/NoteCreate/NoteCreate";
+import {PageNotFound} from "page/PageNotFound/PageNotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NoteBrowse/>}/>
+        <Route path="/note/:id" element={<Note/>}/>
+        <Route path="note/new" element={<NoteCreate/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </Provider>
   </StrictMode>
 );
