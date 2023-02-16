dad661fa78c89ea962436ab617f11f08a89913f9
Aymane Kherrati <68582060+Kerrmane@users.noreply.github.com>
2023-02-16 16:40:00 +0100

    add store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { App } from "App";
 import { StrictMode } from "react";
-import {BrowserRouter, Routes,Route } from "react-router-dom";
-import {NoteBrowse} from "page/NoteBrowse/NoteBrowse";
-import {Note} from "page/Note/Note";
-import {NoteCreate} from "page/NoteCreate/NoteCreate";
-import {PageNotFound} from "page/PageNotFound/PageNotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NoteBrowse } from "pages/NoteBrowse/NoteBrowse";
+import { NoteCreate } from "pages/NoteCreate/NoteCreate";
+import { Note } from "pages/Note/Note";
+import { PageNotFound } from "pages/PageNotFound/PageNotFound";
+import { App } from "App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -16,14 +16,15 @@ root.render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NoteBrowse/>}/>
-        <Route path="/note/:id" element={<Note/>}/>
-        <Route path="note/new" element={<NoteCreate/>}/>
-        <Route path="*" element={<PageNotFound/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<NoteBrowse />} />
+            <Route path="/note/:id" element={<Note />} />
+            <Route path="/note/new" element={<NoteCreate />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
-      
     </Provider>
   </StrictMode>
 );
